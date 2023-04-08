4904035332b0a6c92b7b3872131c979ed7dfd032
Lisa S <halisasaipulla@gmail.com>
2023-04-08 12:28:25 -0700

    double

--- variables_and_basic_data_types/variables.cpp
+++ variables_and_basic_data_types/variables.cpp
@@ -7,9 +7,19 @@ int main(){
     int y = 6;
     int sum = x + y;
 
-    std::cout << x << '\n';
-    std::cout << y << '\n';
-    std::cout << sum << '\n';
+    // std::cout << x << '\n';
+    // std::cout << y << '\n';
+    // std::cout << sum << '\n';
+
+
+    // double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    std::cout << price << '\n';
+    std::cout << gpa << '\n';
+    std::cout << temperature << '\n';
 
     return 0;
 }
