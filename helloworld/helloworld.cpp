782369f235957d4697c71d347c52f2660fc0c04d
Lisa S <halisasaipulla@gmail.com>
2023-04-08 11:47:08 -0700

    added comments

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main(){
+    // std -> standard
+    // cout -> character output
     std::cout << "Hello, world!" ;
+    std::cout << "My name is Lisa." ;
+
+    // endl -> end line output
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "My name is Lisa."<< std::endl;
+
+    // or add new line character
+    std::cout << "Hello, world!" << '\n';
+    std::cout << "My name is Lisa."<< '\n';
+
+    /*
+        return 0; -> if we reach return 0 that means there were no problems in this program, 
+        if return 1,that means there were problems in this program
+    */ 
     return 0;
 }
