828482b6f2222769c8669483e94e25be6626d3f4
Lisa S <halisasaipulla@gmail.com>
2023-04-10 09:15:21 -0700

    Arithmetic operators

---
+++ Arithmetic_operators/main.cpp
@@ -0,0 +1,41 @@
+// arithmetic operators = return the result of a specific arithmetic operation(+,-,*,/)
+// parenthess
+// multiplication & division
+// addition & subtraction
+
+# include <iostream>
+
+int main(){
+
+    // int students = 20;
+    
+    // students = students + 1;
+    // students += 2;
+    // students++;
+
+    // students -= 1;
+    // students--;
+
+    // students = students * 10;
+    // students *= 10;
+
+    // students = students / 10;
+    // students /= 10;
+    // students /= 3;
+    // int reminder = students %3;
+    
+    // double students = 20;
+
+    // students /= 3;
+
+    // parenthess
+    // multiplication & division
+    // addition & subtraction
+
+    int students = 6-5+4+3*(10+2)/2;
+
+    std::cout << students<<'\n';
+    // std::cout << reminder;
+    
+    return 0;
+}
