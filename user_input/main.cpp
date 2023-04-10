78970062c209c6c0dd175c131ee0a97df7c9e02a
Lisa S <halisasaipulla@gmail.com>
2023-04-10 10:49:16 -0700

    User Input

---
+++ user_input/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// cout << (insertion operator)
+// cin << (extraction operator)
+
+int main(){
+    std::string name;
+    int age;
+
+    // std::cout << "What's your name?: ";
+    // std::cin >> name;
+
+    std::cout << "What's your age?: ";
+    std::cin >> age;
+
+    std::cout << "What's your full name?: ";
+    std::getline(std::cin >> std::ws, name); // space between user input
+
+    
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+    return 0;
+}
