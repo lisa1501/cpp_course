83594a191242abb79feb617f6d1309d5411bdf84
Lisa S <halisasaipulla@gmail.com>
2023-04-18 13:46:30 -0700

    For loops

---
+++ For_loops/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    // for(int i=1; i<=3; i++){
+    //     std::cout <<"Hello world! \n";
+    // }
+
+    for(int i=10; i>=0; i-=2){
+        std::cout <<i <<"\n";
+    }
+    std::cout <<"Hello world! \n";
+
+    return 0;
+}
