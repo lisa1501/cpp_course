a5227133dffff1082242038041bfa9893f5bd35b
Lisa S <halisasaipulla@gmail.com>
2023-04-18 13:38:16 -0700

    do while loops

---
+++ Do_while_loops/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    // do while loop = do some block of code first,
+    //                  thne repeat again if condition is true
+
+    int number;
+    //     std::cout << "Enter a positive #: ";
+    //     std::cin >> number;
+
+    // while(number < 0){
+    //     std::cout << "Enter a positive #: ";
+    //     std::cin >> number;
+
+    // }
+
+    do{
+        std::cout << "Enter a positive #: ";
+        std::cin >> number;
+    }while(number < 0);
+    std::cout << "The # is: " << number;
+
+    return 0;
+}
