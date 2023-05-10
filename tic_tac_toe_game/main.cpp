43cbd6b83caac4b2492f3be700e42c68280781a9
Lisa S <halisasaipulla@gmail.com>
2023-05-10 10:29:33 -0700

    tic tac toe game - PlayerMove function

--- tic_tac_toe_game/main.cpp
+++ tic_tac_toe_game/main.cpp
@@ -18,6 +18,11 @@ int main(){
 
     drawBoard(spaces);
 
+    while(running){
+        playerMove(spaces,player);
+        drawBoard(spaces);
+    }
+
     return 0;
 }
 
@@ -35,6 +40,16 @@ void drawBoard(char *spaces){
     std::cout << '\n';
 };
 void playerMove(char *spaces, char player){
+    int number;
+    do{
+        std::cout << "Enter a spot to place a marker (1-9): ";
+        std::cin >> number;
+        number--;
+        if(spaces[number] == ' '){
+            spaces[number] = player;
+            break;
+        }
+    }while(!number > 0 || !number < 8);
 
 };
 void computerMove(char *spaces, char computer){
