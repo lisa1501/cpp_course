973660c7103a34efbe2e3b606b8afd2723859869
Lisa S <halisasaipulla@gmail.com>
2023-05-10 11:06:31 -0700

    tic tac toe game - checkWinner function

--- tic_tac_toe_game/main.cpp
+++ tic_tac_toe_game/main.cpp
@@ -21,9 +21,17 @@ int main(){
     while(running){
         playerMove(spaces,player);
         drawBoard(spaces);
+        if(checkWinner(spaces, player, computer)){
+            running = false;
+            break;
+        }
 
         computerMove(spaces,computer);
         drawBoard(spaces);
+        if(checkWinner(spaces, player, computer)){
+            running = false;
+            break;
+        }
 
     }
 
@@ -70,7 +78,27 @@ void computerMove(char *spaces, char computer){
 
 };
 bool checkWinner(char *spaces, char player, char computer){
-    return 0;
+
+    if((spaces[0]!=' ')&& (spaces[0]== spaces[1]) && (spaces[1]==spaces[2])){
+        spaces[0] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else if((spaces[3]!=' ')&& (spaces[3]== spaces[4]) && (spaces[4]==spaces[5])){
+        spaces[3] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else if((spaces[6]!=' ')&& (spaces[6]== spaces[7]) && (spaces[7]==spaces[8])){
+        spaces[6] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else if((spaces[0]!=' ')&& (spaces[0]== spaces[3]) && (spaces[3]==spaces[6])){
+        spaces[0] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else if((spaces[1]!=' ')&& (spaces[1]== spaces[4]) && (spaces[4]==spaces[7])){
+        spaces[1] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else if((spaces[2]!=' ')&& (spaces[2]== spaces[5]) && (spaces[5]==spaces[8])){
+        spaces[2] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else if((spaces[0]!=' ')&& (spaces[0]== spaces[4]) && (spaces[4]==spaces[8])){
+        spaces[0] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else if((spaces[2]!=' ')&& (spaces[2]== spaces[4]) && (spaces[4]==spaces[6])){
+        spaces[2] == player ? std::cout << "YOU WIN!\n" : std::cout << "YOU LOSE!\n";
+    }else{
+        return false;
+    }
+    return true;
 };
 bool checkTie(char *spaces){
     return 0;
