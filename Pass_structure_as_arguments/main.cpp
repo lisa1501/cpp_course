254bc559624f0b375ccbc37b30982fe728f4513b
Lisa S <halisasaipulla@gmail.com>
2023-05-11 12:10:17 -0700

    Pass structure as arguments

---
+++ Pass_structure_as_arguments/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+struct Car{
+    std::string model;
+    int year;
+    std::string color;
+};
+void printCar(Car &car);
+void paintCar(Car &car, std::string color);
+
+int main(){
+
+    Car car1;
+    Car car2;
+
+    car1.model = "Mustag";
+    car1.year = 2023;
+    car1.color = "red";
+
+    car2.model = "Covette";
+    car2.year = 2024;
+    car2.color = "blue";
+
+    paintCar(car1, "silver");
+    paintCar(car2, "gold");
+
+    std::cout << &car1 << '\n';
+    printCar(car1);
+    printCar(car2);
+
+    return 0;
+}
+
+void printCar(Car &car ){
+    std::cout << &car << '\n';
+    std::cout << car.model << '\n';
+    std::cout << car.year << '\n';
+    std::cout << car.color << '\n';
+
+}
+
+void paintCar(Car &car, std::string color){
+    car.color = color;
+
+}
